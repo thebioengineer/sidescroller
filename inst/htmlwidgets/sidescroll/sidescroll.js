4cc6ce07886056bd04710a6cefc6596afe79a422
thebioengineer <ellishughes@live.com>
2019-11-16 11:03:41 -0800

    update scaling and moving the box to keep toward the top of the screen

--- inst/htmlwidgets/sidescroll/sidescroll.js
+++ inst/htmlwidgets/sidescroll/sidescroll.js
@@ -1,28 +1,33 @@
 
-
-
-var idx;
-
 function doResize(event, el, ui) {
-  
+
     var scale;
-    
-    scale =   Math.min( 
+    var translationY;
+
+    scale =   Math.min(
       ui.body.height / 1000 ,
-      ui.body.width / 1700 
+      ui.body.width / 1700
       );
-  
-  
-  
+
+    console.log(scale);
+
+    translationY =  60 * Math.max(0, 0.9-scale) ;
+
+    console.log(translationY);
+
+
     el.css({
-     transform: "scale(" + scale + ")",
+     transform: "translateY(-" + translationY + "%) " + "scale(" + scale + ") ",
+     position: "relative"
     });
+
 }
 
-function set_slidemaster_size(event,el,ui){
+function set_slide_container_size(event,el,ui){
   el.css({
-    height: ui.body.height ,
-    width: ui.body.width
+    height: ui.body.height * .98, /* make a scoche smaller than the body */
+    width: ui.body.width * .98,
+    margin: "auto"
   });
 }
 
@@ -37,67 +42,63 @@ $(document).ready(function(){
 			centerMode: true,
 			variableWidth: true
 			});
-			
+
 		var $slide_wrapper = $(".slide_master_wrapper")
 		var $slide_master = $(".slide_master")
 	  var $wrapper = $("body");
-	  set_slidemaster_size(null,
-	    $slide_wrapper,
-		  { body: {
-		      height: window.innerHeight*.95,
-		      width: window.innerWidth
-		    }
-		  });
-		 set_slidemaster_size(null,
-	    $slide_master,
-		  { body: {
-		      height: window.innerHeight*.95,
-		      width: window.innerWidth
-		    }
-		  });
-	  
-		doResize(null,
+
+	  set_slide_container_size(null,
 		  $slide_wrapper,
 		  { body: {
 		      height: window.innerHeight,
 		      width: window.innerWidth
 		    }
+		});
+
+		doResize(null,
+		  $slide_master,
+		  { body: {
+		      height: window.innerHeight,
+		      width: window.innerWidth
+		    }
 		  });
-	
+
+
+
 	$('.slide_master').on('afterChange', function(event, slick, currentSlide, nextSlide){
     console.log("Viz Me!");
     var slide = $(".slick-current").find(".slide_container");
     slide.css('visibility','visible');
   });
-    
+
 });
 
 
 
 $(window).resize(function() {
-  var $slide_wrapper = $(".slide_master_wrapper");
-	var $slide_master = $(".slide_master");
-	var $wrapper = $("body");
-	//set_slidemaster_size(null,
-	//    $slide_wrapper,
-	//	  { body: {
-	//	      height: "900px",
-	//	      width: "1600px"
-	//	    }
-	//	  });
-	doResize(null,
-		    $slide_wrapper,
-		    { body: {
+  	var $slide_wrapper = $(".slide_master_wrapper")
+		var $slide_master = $(".slide_master")
+	  var $wrapper = $("body");
+
+		doResize(null,
+		  $slide_master,
+		  { body: {
 		      height: window.innerHeight,
 		      width: window.innerWidth
 		    }
-		    });
-  
-  
+		  });
+		set_slide_container_size(null,
+		  $slide_wrapper,
+		  { body: {
+		      height: window.innerHeight,
+		      width: window.innerWidth
+		    }
+		});
+
     //var idx = $(".slick-current").index();
 		//$('.slide_master').slick("unslick");
 		//window.setTimeout(function(){
-		//  
+		//
   	//	$('.slide_master').show(); // optionally
   	//	//$('.slide_master').slick({
   	//	//	accessibility: true,
@@ -109,7 +110,7 @@ $(window).resize(function() {
   	//	//	variableWidth: true,
   	//	//  initialSlide: idx
   	//	//});
-  	//	
+  	//
     //
     //
     //}, 200); // can be changed to a lower value - whatever works
